5f769d54bc3142bde832bb40d4ba4e54e5d248ae
djkreyz <recon2212@yandex.ru>
2021-03-16 10:14:41 +0300

    Simple "Timer" script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    Text timeText;
+    float currentTime = 0;
+    bool timerIsRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeText = GetComponent<Text>();
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            currentTime += Time.deltaTime;
+        }
+
+        DisplayTime(currentTime);
+    }
+
+    private void DisplayTime(float currentTime)
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
