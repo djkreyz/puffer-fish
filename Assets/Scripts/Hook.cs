650d07e0d9ea811c15863459aebedd116d64fd9a
djkreyz <recon2212@yandex.ru>
2021-03-19 15:30:34 +0300

    enemy - rope

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    float actualRopeSize;
+    float ropeTimer;
+
+    void Start()
+    {
+       
+    }
+
+    
+    void Update()
+    {
+        RopeMovement();
+    }
+
+    private void RopeMovement()
+    {
+        ropeTimer += 0.1f;
+        Debug.Log(ropeTimer);
+
+
+        if (ropeTimer <= 100f)
+        {
+            GoDown();
+        }
+        else
+        {
+            GoUp();
+        }
+    }
+
+    private void GoDown()
+    {
+        gameObject.transform.localScale += new Vector3(0, 1f, 0) * Time.deltaTime;
+    }
+
+    private void GoUp()
+    {
+        actualRopeSize = gameObject.transform.localScale.y;
+
+        gameObject.transform.localScale -= new Vector3(0, 2.5f, 0) * Time.deltaTime;
+        if (actualRopeSize <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
